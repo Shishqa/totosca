3d388b854e393c9562b8f127e7bc803dd0ddb519
shishqa <shishqa.main@gmail.com>
2024-01-13 23:45:40 +0300

    feat: add function parsing

--- crates/toto_parser/src/tosca/v1_3/mod.rs
+++ crates/toto_parser/src/tosca/v1_3/mod.rs
@@ -1,3 +1,5 @@
+use toto_tosca::{Entity, Relation};
+
 use crate::parse::{Context, GraphHandle};
 
 use super::{v2_0, Parse, ToscaDefinitionsVersion};
@@ -11,7 +13,7 @@ pub mod value;
 impl ToscaDefinitionsVersion for Tosca1_3 {
     type AttributeDefinition = v2_0::AttributeDefinition;
     type AttributeAssignment = attribute::AttributeAssignment;
-    type PropertyAssignment = v2_0::PropertyAssignment;
+    type PropertyAssignment = value::Value;
     type PropertyDefinition = v2_0::PropertyDefinition;
     type ParameterDefinition = v2_0::ParameterDefinition;
     type FileDefinition = file::ToscaFileDefinition;
@@ -26,6 +28,22 @@ impl ToscaDefinitionsVersion for Tosca1_3 {
     type Value = value::Value;
 
     fn parse(ctx: &mut Context, n: &yaml_peg::NodeRc) -> GraphHandle {
+        let root = ctx.graph.add_node(Entity::File);
+        let builtin_url = ctx.graph.add_node(Entity::String("$builtin".to_string()));
+        ctx.graph.add_edge(root, builtin_url, Relation::Url);
+
+        let f = ctx.graph.add_node(Entity::Function);
+        ctx.graph
+            .add_edge(root, f, Relation::Subdef("get_input".to_string()));
+
+        let f = ctx.graph.add_node(Entity::Function);
+        ctx.graph
+            .add_edge(root, f, Relation::Subdef("get_attribute".to_string()));
+
+        let f = ctx.graph.add_node(Entity::Function);
+        ctx.graph
+            .add_edge(root, f, Relation::Subdef("get_property".to_string()));
+
         return Self::FileDefinition::parse::<Self>(ctx, n);
     }
 }
