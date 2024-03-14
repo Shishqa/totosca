681cf7701ee67b3a055ec7d2e90d08c7a99c2567
shishqa <shishqa.main@gmail.com>
2024-03-14 22:55:11 +0300

    wip

--- crates/toto_ast/src/lib.rs
+++ crates/toto_ast/src/lib.rs
@@ -2,7 +2,3 @@ use petgraph::{Directed, Graph};
 
 pub type AST<E, R> = Graph<E, R, Directed, u32>;
 pub type GraphHandle = petgraph::graph::NodeIndex<u32>;
-
-pub trait Parse<E, R> {
-    fn parse(self, ast: &mut AST<E, R>) -> GraphHandle;
-}
