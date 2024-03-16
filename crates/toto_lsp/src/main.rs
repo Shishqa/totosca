ab149761bb80f16e77497bc1568d480d55ba3fc2
shishqa <shishqa.main@gmail.com>
2024-03-16 11:51:34 +0300

    wip: preserve ast in lsp server

--- crates/toto_lsp/src/main.rs
+++ crates/toto_lsp/src/main.rs
@@ -4,6 +4,7 @@ use graphviz_rust::cmd::{CommandArg, Format};
 use graphviz_rust::printer::PrinterContext;
 use lsp_types::notification::Notification;
 
+use lsp_types::request::Request;
 use petgraph::visit::EdgeRef;
 use serde_json::from_value;
 
@@ -19,163 +20,194 @@ fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
     // Note that  we must have our logging only write out to stderr.
     eprintln!("starting generic LSP server");
 
-    // Create the transport. Includes the stdio (stdin and stdout) versions but this could
-    // also be implemented to use sockets or HTTP.
-    let (connection, io_threads) = lsp_server::Connection::stdio();
-
-    // Run the server and wait for the two threads to end (typically by trigger LSP Exit event).
-    let server_capabilities = serde_json::to_value(lsp_types::ServerCapabilities {
-        text_document_sync: Some(lsp_types::TextDocumentSyncCapability::Kind(
-            lsp_types::TextDocumentSyncKind::INCREMENTAL,
-        )),
-        // diagnostic_provider: Some(DiagnosticServerCapabilities::Options(DiagnosticOptions {
-        //     identifier: None,
-        //     inter_file_dependencies: false,
-        //     workspace_diagnostics: false,
-        //     work_done_progress_options: WorkDoneProgressOptions {
-        //         work_done_progress: None,
-        //     },
-        // })),
-        ..Default::default()
-    })
-    .unwrap();
-    let initialization_params = match connection.initialize(server_capabilities) {
-        Ok(it) => it,
-        Err(e) => {
-            if e.channel_is_disconnected() {
-                io_threads.join()?;
-            }
-            return Err(e.into());
-        }
-    };
-    main_loop(connection, initialization_params)?;
-    io_threads.join()?;
+    let server = Server::new();
+    server.run()?;
 
     // Shut down gracefully.
     eprintln!("shutting down server");
     Ok(())
 }
 
-fn refresh_diag(
-    connection: &lsp_server::Connection,
-    uri: &url::Url,
-) -> Result<(), Box<dyn Error + Sync + Send>> {
-    eprintln!("trying read: {uri:?}");
-
-    let mut ast = toto_ast::AST::<Entity, Relation>::new();
-
-    let mut doc = toto_yaml::FileEntity::from_url(uri.clone());
-    doc.fetch().unwrap();
-
-    let doc_handle = ast.add_node(doc.into());
-    let doc_root = toto_yaml::YamlParser::parse(doc_handle, &mut ast).unwrap();
-    ToscaParser::parse(doc_root, &mut ast);
-
-    let doc = ast.node_weight(doc_handle).unwrap().as_file().unwrap();
-    let diagnostics: Vec<lsp_types::Diagnostic> = get_errors(&ast)
-        .filter_map(|(what, loc)| {
-            let len = get_yaml_len(loc, &ast);
-            let (pos, file) = ast
-                .edges(loc)
-                .find_map(|e| match e.weight().as_file() {
-                    Some(pos) => Some((pos.0, e.target())),
-                    _ => None,
-                })
-                .unwrap();
-            if file != doc_handle {
-                return None;
-            }
-
-            let get_lc = |offset| -> (u32, u32) {
-                let linebreaks = doc.content.as_ref().unwrap()[0..offset]
-                    .chars()
-                    .enumerate()
-                    .filter_map(|c| if c.1 == '\n' { Some(c.0) } else { None })
-                    .collect::<Vec<_>>();
-                let lineno = linebreaks.len();
-                let charno = pos - linebreaks.iter().next_back().copied().unwrap_or_default() - 1;
-                (lineno as u32, charno as u32)
-            };
-
-            let (lineno_start, charno_start) = get_lc(pos);
-            let (lineno_end, charno_end) = get_lc(pos + len);
-
-            Some(lsp_types::Diagnostic::new(
-                lsp_types::Range {
-                    start: lsp_types::Position {
-                        line: lineno_start,
-                        character: charno_start,
-                    },
-                    end: lsp_types::Position {
-                        line: lineno_end,
-                        character: charno_end,
-                    },
-                },
-                None,
-                None,
-                None,
-                format!("{:?}", ast.node_weight(what).unwrap().as_parse().unwrap()),
-                None,
-                None,
-            ))
-        })
-        .collect();
-
-    let notif_params = Some(lsp_types::PublishDiagnosticsParams {
-        uri: uri.clone(),
-        version: None,
-        diagnostics,
-    });
-    let notif_params = serde_json::to_value(notif_params)?;
-
-    let notif = lsp_server::Message::Notification(lsp_server::Notification {
-        method: lsp_types::notification::PublishDiagnostics::METHOD.into(),
-        params: notif_params,
-    });
-
-    eprintln!("sending: {notif:?}");
-
-    connection.sender.send(notif)?;
-
-    Ok(())
+struct Server {
+    connection: lsp_server::Connection,
+    io_threads: lsp_server::IoThreads,
+    ast: toto_ast::AST<Entity, Relation>,
 }
 
-fn main_loop(
-    connection: lsp_server::Connection,
-    params: serde_json::Value,
-) -> Result<(), Box<dyn Error + Sync + Send>> {
-    let _params: lsp_types::InitializeParams = serde_json::from_value(params).unwrap();
-    eprintln!("starting example main loop");
-    for msg in &connection.receiver {
-        eprintln!("got msg: {msg:?}");
-        match msg {
-            lsp_server::Message::Request(req) => {
-                if connection.handle_shutdown(&req)? {
-                    return Ok(());
-                }
-                eprintln!("got request: {req:?}");
-            }
-            lsp_server::Message::Response(resp) => {
-                eprintln!("got response: {resp:?}");
-            }
-            lsp_server::Message::Notification(not) => {
-                eprintln!("got notification: {not:?}");
+impl Server {
+    pub fn new() -> Self {
+        // Create the transport. Includes the stdio (stdin and stdout) versions but this could
+        // also be implemented to use sockets or HTTP.
+        let (connection, io_threads) = lsp_server::Connection::stdio();
+        Self {
+            connection,
+            io_threads,
+            ast: toto_ast::AST::<Entity, Relation>::new(),
+        }
+    }
 
-                match not.method.as_str() {
-                    lsp_types::notification::DidOpenTextDocument::METHOD => {
-                        let params: lsp_types::DidOpenTextDocumentParams = from_value(not.params)?;
-                        refresh_diag(&connection, &params.text_document.uri)?;
-                        continue;
+    pub fn run(mut self) -> Result<(), Box<dyn Error + Sync + Send>> {
+        let server_capabilities = serde_json::to_value(lsp_types::ServerCapabilities {
+            text_document_sync: Some(lsp_types::TextDocumentSyncCapability::Kind(
+                lsp_types::TextDocumentSyncKind::INCREMENTAL,
+            )),
+            // diagnostic_provider: Some(DiagnosticServerCapabilities::Options(DiagnosticOptions {
+            //     identifier: None,
+            //     inter_file_dependencies: false,
+            //     workspace_diagnostics: false,
+            //     work_done_progress_options: WorkDoneProgressOptions {
+            //         work_done_progress: None,
+            //     },
+            // })),
+            ..Default::default()
+        })
+        .unwrap();
+        let initialization_params = match self.connection.initialize(server_capabilities) {
+            Ok(it) => it,
+            Err(e) => {
+                if e.channel_is_disconnected() {
+                    self.io_threads.join()?;
+                }
+                return Err(e.into());
+            }
+        };
+        let _params: lsp_types::InitializeParams = serde_json::from_value(initialization_params)?;
+        eprintln!("starting example main loop");
+        for msg in &self.connection.receiver.clone() {
+            eprintln!("got msg: {msg:?}");
+            match msg {
+                lsp_server::Message::Request(req) => {
+                    if self.connection.handle_shutdown(&req)? {
+                        return Ok(());
                     }
-                    lsp_types::notification::DidSaveTextDocument::METHOD => {
-                        let params: lsp_types::DidSaveTextDocumentParams = from_value(not.params)?;
-                        refresh_diag(&connection, &params.text_document.uri)?;
-                        continue;
+
+                    match req.method.as_str() {
+                        lsp_types::request::GotoDefinition::METHOD => {
+                            let params: lsp_types::GotoDefinitionParams = from_value(req.params)?;
+                            self.goto_definition(&params.text_document_position_params)?;
+                            continue;
+                        }
+                        &_ => {}
                     }
-                    &_ => {}
+
+                    eprintln!("got request: {req:?}");
+                }
+                lsp_server::Message::Response(resp) => {
+                    eprintln!("got response: {resp:?}");
+                }
+                lsp_server::Message::Notification(not) => {
+                    eprintln!("got notification: {not:?}");
+
+                    match not.method.as_str() {
+                        lsp_types::notification::DidOpenTextDocument::METHOD => {
+                            let params: lsp_types::DidOpenTextDocumentParams =
+                                from_value(not.params)?;
+                            self.refresh_diag(&params.text_document.uri)?;
+                            continue;
+                        }
+                        lsp_types::notification::DidSaveTextDocument::METHOD => {
+                            let params: lsp_types::DidSaveTextDocumentParams =
+                                from_value(not.params)?;
+                            self.refresh_diag(&params.text_document.uri)?;
+                            continue;
+                        }
+                        &_ => {}
+                    }
                 }
             }
         }
+        Ok(())
     }
-    Ok(())
+
+    fn refresh_diag(&mut self, uri: &url::Url) -> Result<(), Box<dyn Error + Sync + Send>> {
+        eprintln!("trying read: {uri:?}");
+
+        self.ast.clear();
+
+        let mut doc = toto_yaml::FileEntity::from_url(uri.clone());
+        doc.fetch().unwrap();
+
+        let doc_handle = self.ast.add_node(doc.into());
+        let doc_root = toto_yaml::YamlParser::parse(doc_handle, &mut self.ast).unwrap();
+        ToscaParser::parse(doc_root, &mut self.ast);
+
+        let doc = self.ast.node_weight(doc_handle).unwrap().as_file().unwrap();
+        let diagnostics: Vec<lsp_types::Diagnostic> = get_errors(&self.ast)
+            .filter_map(|(what, loc)| {
+                let len = get_yaml_len(loc, &self.ast);
+                let (pos, file) = self
+                    .ast
+                    .edges(loc)
+                    .find_map(|e| match e.weight().as_file() {
+                        Some(pos) => Some((pos.0, e.target())),
+                        _ => None,
+                    })
+                    .unwrap();
+                if file != doc_handle {
+                    return None;
+                }
+
+                let get_lc = |offset| -> (u32, u32) {
+                    let linebreaks = doc.content.as_ref().unwrap()[0..offset]
+                        .chars()
+                        .enumerate()
+                        .filter_map(|c| if c.1 == '\n' { Some(c.0) } else { None })
+                        .collect::<Vec<_>>();
+                    let lineno = linebreaks.len();
+                    let charno =
+                        pos - linebreaks.iter().next_back().copied().unwrap_or_default() - 1;
+                    (lineno as u32, charno as u32)
+                };
+
+                let (lineno_start, charno_start) = get_lc(pos);
+                let (lineno_end, charno_end) = get_lc(pos + len);
+
+                Some(lsp_types::Diagnostic::new(
+                    lsp_types::Range {
+                        start: lsp_types::Position {
+                            line: lineno_start,
+                            character: charno_start,
+                        },
+                        end: lsp_types::Position {
+                            line: lineno_end,
+                            character: charno_end,
+                        },
+                    },
+                    None,
+                    None,
+                    None,
+                    format!(
+                        "{:?}",
+                        self.ast.node_weight(what).unwrap().as_parse().unwrap()
+                    ),
+                    None,
+                    None,
+                ))
+            })
+            .collect();
+
+        let notif_params = Some(lsp_types::PublishDiagnosticsParams {
+            uri: uri.clone(),
+            version: None,
+            diagnostics,
+        });
+        let notif_params = serde_json::to_value(notif_params)?;
+
+        let notif = lsp_server::Message::Notification(lsp_server::Notification {
+            method: lsp_types::notification::PublishDiagnostics::METHOD.into(),
+            params: notif_params,
+        });
+
+        eprintln!("sending: {notif:?}");
+        self.connection.sender.send(notif)?;
+
+        Ok(())
+    }
+
+    fn goto_definition(
+        &self,
+        params: &lsp_types::TextDocumentPositionParams,
+    ) -> Result<(), Box<dyn Error + Sync + Send>> {
+        Ok(())
+    }
 }
